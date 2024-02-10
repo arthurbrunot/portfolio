b3de7ec3e314b0264230703a8f8ba2c0d9fa15b8
abrunot <a.brunot@debussac.net>
2024-02-10 21:23:56 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { type Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { RootLayout } from '@/components/RootLayout'
+
+const inter = Inter({ subsets: ['latin'] })
+import '@/styles/tailwind.css'
+import clsx from "clsx";
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Studio',
+    default: 'Studio - Award winning developer studio based in Denmark',
+  },
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fr" className={clsx("h-full bg-neutral-950 text-base antialiased", inter.className)}>
+      <body className="flex min-h-full flex-col">
+        <RootLayout>{children}</RootLayout>
+      </body>
+    </html>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,50 @@
+import { type Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+  content: ['./src/**/*.{js,jsx,mdx,ts,tsx}'],
+  theme: {
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '2rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2.25rem' }],
+      '3xl': ['1.75rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2rem', { lineHeight: '2.5rem' }],
+      '5xl': ['2.5rem', { lineHeight: '3rem' }],
+      '6xl': ['3rem', { lineHeight: '3.5rem' }],
+      '7xl': ['4rem', { lineHeight: '4.5rem' }],
+    },
+    extend: {
+      borderRadius: {
+        '4xl': '2.5rem',
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        display: [
+          ['Inter', ...defaultTheme.fontFamily.sans],
+          { fontVariationSettings: '"wdth" 125' },
+        ],
+      },
+      colors: {
+        'primary': {
+          '50': '#f9ffe4',
+          '100': '#f0ffc4',
+          '200': '#e0ff90',
+          '300': '#c8ff50',
+          '400': '#baff39',
+          '500': '#90e600',
+          '600': '#6fb800',
+          '700': '#538b00',
+          '800': '#436d07',
+          '900': '#395c0b',
+          '950': '#1c3400',
+          DEFAULT: '#baff39',
+        },
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
